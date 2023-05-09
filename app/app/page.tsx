ad114cafc505b1faa01172b565b2153bdc899f27
Austin Wilke <99009551+Wilkeaustin@users.noreply.github.com>
2023-05-09 16:46:34 -0500

    first commit

---
+++ app/app/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React from 'react';
+
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+
+
+import { format } from 'date-fns';
+
+export default function IndexPage() {
+  const [selected, setSelected] = React.useState<Date>();
+
+  let footer = <p>Please pick a day.</p>;
+  if (selected) {
+    footer = <p>You picked {format(selected, 'PP')}.</p>;
+  }
+  return (
+    <DayPicker
+      mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      footer={footer}
+    />
+  );
+}

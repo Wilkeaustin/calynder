8c81ba9a891e732aa14071580cbe1a6ab956f4f4
Austin Wilke <99009551+Wilkeaustin@users.noreply.github.com>
2023-05-09 16:53:57 -0500

    Update page.tsx
    changing title

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function IndexPage() {
   <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
     <div className="mx-auto max-w-xl text-center">
       <h1 className="text-3xl font-extrabold sm:text-5xl">
-        Understand User Flow.
+        Calynder is great
         <strong className="font-extrabold text-red-700 sm:block">
           Increase Conversion.
         </strong>
